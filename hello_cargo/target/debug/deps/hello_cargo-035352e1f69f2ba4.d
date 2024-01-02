5823fd51b376bb3df84296c232bf8a93860156b6
Animesh Kumar <animesh@smartreach.io>
2024-01-02 17:20:15 +0530

    Using cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-035352e1f69f2ba4.d
@@ -0,0 +1,5 @@
+/Users/animeshkumar/Documents/projects/rust_training_animesh/hello_cargo/target/debug/deps/hello_cargo-035352e1f69f2ba4: src/main.rs
+
+/Users/animeshkumar/Documents/projects/rust_training_animesh/hello_cargo/target/debug/deps/hello_cargo-035352e1f69f2ba4.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/animeshkumar/Documents/projects/rust_training_animesh/hello_cargo/target/debug/hello_cargo: /Users/animeshkumar/Documents/projects/rust_training_animesh/hello_cargo/src/main.rs
